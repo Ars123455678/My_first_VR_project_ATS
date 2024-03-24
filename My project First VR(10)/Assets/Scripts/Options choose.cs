3f9e6847d7bf180dbe8c161374190afc267f77a4
Ars123455678 <twotservice@yandex.ru>
2024-03-24 13:41:21 +0300

    Three rooms

---
+++ My project First VR(10)/Assets/Scripts/Options choose.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Optionschoos : TurbinaComponent
+{
+	public GameObject prefab;
+
+	public void FirstRoom() {
+		prefab.transform.position = new Vector3(3, 1, 4);
+	}
+	public void SecondRoom() {
+		prefab.transform.position = new Vector3(3, 1, 16);
+	}
+	public void ThirdRoom() {
+		prefab.transform.position = new Vector3(3, 1, 28);
+	}
+}
