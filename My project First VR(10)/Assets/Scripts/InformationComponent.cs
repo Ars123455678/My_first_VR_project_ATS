48512b3f953d9f3d7525ccd6b233d0d99f2029a8
Ars123455678 <twotservice@yandex.ru>
2024-03-24 16:42:50 +0300

    Name

---
+++ My project First VR(10)/Assets/Scripts/InformationComponent.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InformationComponent : MonoBehaviour
+{
+    private GameObject textGo;
+    private XRSimpleInteractable interactable;
+
+    private void Start()
+    {
+        textGo = transform.GetChild(0).gameObject;
+        interactable = GetComponent<XRSimpleInteractable>();
+        HideText(null);
+        interactable.hoverEntered.AddListener(ShowText);
+        interactable.hoverExited.AddListener(HideText); 
+    } 
+
+    public void ShowText(HoverEnterEventArgs f)
+    {
+        textGo.SetActive(true);
+    }
+
+    public void HideText(HoverExitEventArgs f)
+    {
+        textGo.SetActive(false);
+    }
+}
