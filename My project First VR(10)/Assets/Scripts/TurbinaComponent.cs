4a1c0fbf8855c4b4a1e77c74eefe0dccb8d98991
PapusGames <ruzmanov07@bk.ru>
2024-03-24 14:52:40 +0300

    Text appearence

---
+++ My project First VR(10)/Assets/Scripts/TurbinaComponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;		   
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TurbinaComponent : MonoBehaviour
+{ 
+	public bool isDropped;
+	protected XRGrabInteractable grab;
+	
+
+	protected virtual void Start() {
+		grab = GetComponent<XRGrabInteractable>();
+	} 
+}
